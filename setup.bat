19eb344158521fad0fc1afa2b09b2496fb1b8ef8
Bot007bot <36933522+Bot007bot@users.noreply.github.com>
2018-02-28 21:52:26 +0200

    Changed bats

--- setup.bat
+++ setup.bat
@@ -5,9 +5,8 @@ cd "PhotoMaker"
 powershell Start-BitsTransfer -Source "https://www.python.org/ftp/python/3.6.4/python-3.6.4.exe" -Destination "%USERPROFILE%\AppData\Local\PhotoMaker\python-3.6.4.exe"
 python-3.6.4.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 del "%USERPROFILE%\AppData\Local\PhotoMaker\python-3.6.4.exe"
-pip install opencv-python
-pip install email
-powershell Start-BitsTransfer -Source "script.py" -Destination "%USERPROFILE%\AppData\Local\PhotoMaker\script.py"
-powershell Start-BitsTransfer -Source "start.bat" -Destination "%USERPROFILE%\AppData\Local\PhotoMaker\start.bat"
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('"%USERPROFILE%\Start Menu\Programs\Startup\start.lnk");$s.TargetPath="start.bat";$s.Save()"
-py -3 "%USERPROFILE%\AppData\Local\PhotoMaker\script.py"
+powershell Start-BitsTransfer -Source "https://raw.githubusercontent.com/Bot007bot/forfun/master/setup2.bat" -Destination "%USERPROFILE%\AppData\Local\PhotoMaker\script.py"
+.\setup2.bat
+pause
+
+
---
+++ setup2.bat
@@ -0,0 +1,7 @@
+pip install opencv-python
+pip install email
+powershell Start-BitsTransfer -Source "https://raw.githubusercontent.com/Bot007bot/forfun/master/script.py" -Destination "%USERPROFILE%\AppData\Local\PhotoMaker\script.py"
+powershell Start-BitsTransfer -Source "https://raw.githubusercontent.com/Bot007bot/forfun/master/start.bat" -Destination "%USERPROFILE%\AppData\Local\PhotoMaker\start.bat"
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('"%USERPROFILE%\Start Menu\Programs\Startup\start.lnk");$s.TargetPath="start.bat";$s.Save()"
+py -3 "%USERPROFILE%\AppData\Local\PhotoMaker\script.py"
+pause
