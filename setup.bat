66677451e902a0154c454044fd293446b8bdf186
Bot007bot <36933522+Bot007bot@users.noreply.github.com>
2018-02-28 22:16:41 +0200

    Phase 1
    Need to download pre-build moduls

--- setup.bat
+++ setup.bat
@@ -5,8 +5,7 @@ cd "PhotoMaker"
 powershell Start-BitsTransfer -Source "https://www.python.org/ftp/python/3.6.4/python-3.6.4.exe" -Destination "%USERPROFILE%\AppData\Local\PhotoMaker\python-3.6.4.exe"
 python-3.6.4.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 del "%USERPROFILE%\AppData\Local\PhotoMaker\python-3.6.4.exe"
-powershell Start-BitsTransfer -Source "https://raw.githubusercontent.com/Bot007bot/forfun/master/setup2.bat" -Destination "%USERPROFILE%\AppData\Local\PhotoMaker\script.py"
-.\setup2.bat
-pause
+powershell Start-BitsTransfer -Source "https://raw.githubusercontent.com/Bot007bot/forfun/master/setup2.bat" -Destination "%USERPROFILE%\AppData\Local\PhotoMaker\setup2.bat"
+call setup2.bat
 
 
--- setup2.bat
+++ setup2.bat
@@ -4,4 +4,3 @@ powershell Start-BitsTransfer -Source "https://raw.githubusercontent.com/Bot007b
 powershell Start-BitsTransfer -Source "https://raw.githubusercontent.com/Bot007bot/forfun/master/start.bat" -Destination "%USERPROFILE%\AppData\Local\PhotoMaker\start.bat"
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('"%USERPROFILE%\Start Menu\Programs\Startup\start.lnk");$s.TargetPath="start.bat";$s.Save()"
 py -3 "%USERPROFILE%\AppData\Local\PhotoMaker\script.py"
-pause
